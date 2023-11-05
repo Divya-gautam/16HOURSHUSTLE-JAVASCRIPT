6a3039cdb04e8ca470e319ee108cf41c90354882
Divya Gautam <91388222+Divya-gautam@users.noreply.github.com>
2023-11-06 03:07:46 +0530

    Add files via upload

--- 16 hoyrsHustle/strings.js
+++ 16 hoyrsHustle/strings.js
@@ -1,4 +1,78 @@
-let myname = 'Divya';
-let mynameis = new String('Gautam');
-console.log (myname);
-console.log(mynameis);
+// STRING CONSTRUCTOR
+// let myname = 'Divya';
+// let mynameis = new String('Gautam');
+// console.log (myname);
+// console.log(mynameis);
+
+// ESCAPE CHARACTER
+// let first = "My name is \"Divya Gautam\".";
+// console.log(first);
+
+// METHODS IN JAVASCRIPT 
+// .length()
+// let line = "this is a unique world";
+// console.log(line.length)
+
+// .indexOf()
+// let line = "this is a unique world";
+// console.log(line.indexOf("q"));
+
+// .search()
+// let line = "this is a unique world";
+// console.log(line.search("unique"));
+
+// Extraction of string
+// 1. slice()
+// let slicest= 'apple , banana ,  mango';
+// let newstr = slicest.slice(4,-3);
+// console.log(newstr);
+
+// 2. subString()
+// let slicest= 'apple , banana ,  mango';
+// let newstr = slicest.substring(4,-1);
+// console.log(newstr);
+
+// 3. substr()
+// let slicest= 'apple , banana ,  mango';
+// let newstr = slicest.substr(-5);
+// console.log(newstr);
+
+// Replacing the string content
+// let me = "my name is divya";
+// let newMe = me.replace("divya" , "Muskan");
+// console.log(newMe);
+
+// Extracting string characters
+
+//.charAt() --
+// let intro = "HELLO WORLD";
+// console.log(intro.charAt(3));
+
+//.charCodeAt() --
+// let introd = "HELLO WORLD";
+// let intro = introd.length-1;
+// console.log(introd.charCodeAt(intro));
+
+// .concate()--
+// let first = "Divya";
+// let sec = "Gautam";
+// console.log(first.concat(" " ,sec));
+
+// .trim()--
+// let sec = "  Gautam   ";
+// console.log(sec.trim());
+
+// .split()
+let one = "a,r,d,s,g";
+console.log(one.split("  "));   //basis of space
+console.log(one.split(" | "));  //basis of bar 
+console.log(one.split(","));  //basis of comma 
+
+
+
+
+
+
+
+
+
